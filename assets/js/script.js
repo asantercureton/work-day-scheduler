93b2f27c01cdac3b84e9fb00b7280a007a65c32f
Asante Cureton <asantercureton@gmail.com>
2021-08-29 18:44:36 -0400

    Updated change state of textarea and worked on loop to change the remaining textareas

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,32 +2,49 @@ var container = document.querySelector('container');
 var saveBtn = document.querySelectorAll('.col-sm-1 saveBtn');
 var textInput = document.querySelectorAll('.col-sm-10 description');
 var currentDate = moment().format("dddd, MMMM Do");
-var rowTime = document.querySelectorAll('time-block');
+// var rowTime = document.getElementsByClassName('time-block').childElementCount;
+var rowTime = ["8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+var rowArray = document.getElementsByClassName('col-sm-1 hour')[0];
 
+// LOOP
+for (var i = 0; i < rowTime.length; i++) {
 
-console.log("RV", rowTime);
-console.log("CURR", currentDate);
+    time = rowTime[i];
+    // Current time AM/PM
+    var now = new moment();
+    console.log("hA", now.format("hA"));
+    console.log("rowTime", rowTime);
+    // Change state of textarea
+    if (now.format("hA") > rowTime) {
 
+        // Changing color green
+        // console.log(rowArray.innerHTML, now.format("hA"));
 
-// Create alternating timeblock colors for each day
-
+        document.getElementById("time").style.backgroundColor = "green";
+    } else {
+        // Change color red
+        document.getElementById("time").style.backgroundColor = "red";
+    }
+}
 
 // Display the current date
-document.getElementById('currentDay').innerHTML= currentDate;
+document.getElementById('currentDay').innerHTML = currentDate;
 
 
-function returnText(){
-    // let input = document.getElementById("textInput")
-    alert("textInput")
-}
-// var currentTime = moment().format("H");
-// Display state: past, present, future
+// function returnText(){
+//     // let input = document.getElementById("textInput")
+//     alert("textInput")
+// }
 
-console.log("HELLO");
+// console.log("rowArray", rowArray.innerHTML);
+
+console.log(rowTime);
+
+// console.log("HELLO");
 function changeState() {
     // if (currentDate > columnTime) {
     //     // Changing color green
-        
+
     //     document.getElementById("time").style.backgroundColor = "green";
     // } else {
     //     // Change color red
@@ -35,21 +52,12 @@ function changeState() {
     // }
 };
 // var formatDate = "dddd MMMM Mo";
-var formatDate = columnTime;
-console.log("TIME", formatDate);
+// console.log("TIME", rowTime);
 // Change the color of the textarea to indicate state: past(red), present(default color), future(green)
-if (currentDate > formatDate) {
-    // Changing color green
-    
-    document.getElementById("time").style.backgroundColor = "green";
-} else {
-    // Change color red
-    document.getElementById("time").style.backgroundColor = "red";
-}
 
 // Looping through time blocks
-var timeBlock = [...formatDate];
-console.log("Block", timeBlock);
+// var timeBlock = [...rowTime];
+// console.log("Block", timeBlock);
 
 // columnTime.forEach(changeState);
 
